5a2607008178e79baf1b6bb2976f7ed7b87686de
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-05-19 10:34:01 +0530

    Add files via upload

---
+++ saral3.py
@@ -0,0 +1,7 @@
+import json
+dict={"name":"muskan","class":"12th","age":18,"study":"softwere engineering"}
+json_string=json.dumps(dict)
+print(type(json_string))
+print(json_string)
+# python to json string convert
+
