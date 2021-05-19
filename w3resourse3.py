d84748f8b7b75bbb35344ade6937938420abffb8
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-05-19 10:39:33 +0530

    Add files via upload

---
+++ w3resourse3.py
@@ -0,0 +1,26 @@
+import json
+python_dict =  {"name": "David", "age": 6, "class":"I"}
+python_list =  ["Red", "Green", "Black"]
+python_str =  "Python Json"
+python_int =  (1234)
+python_float =  (21.34)
+python_T =  (True)
+python_F =  (False)
+python_N =  (None)
+a=json.dumps(python_dict)
+b=json.dumps(python_list)
+c=json.dumps(python_str)
+d=json.dumps(python_int)
+e=json.dumps(python_float)
+f=json.dumps(python_T)
+g=json.dumps(python_F)
+h=json.dumps(python_N)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
